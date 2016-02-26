190f11788a831de65b303973fc20f7b9694a0f17
bgulbis <brian.gulbis@gmail.com>
2016-02-26 09:57:17 -0600

    get encounter list

--- library.R
+++ library.R
@@ -3,6 +3,7 @@
 library(readxl)
 library(dplyr)
 library(stringr)
+library(BGTools)
 
 # set directories
 pt.dir <- "Patients"
--- read_data.R
+++ read_data.R
@@ -5,6 +5,8 @@
 
 source("library.R")
 
+# excel data ----
+
 # read in patient list from Excel; keep only patients being validated
 xls.name <- paste(pt.dir, "chads hasbled validation.xls", sep = "/")
 pts <- read_excel(xls.name) %>%
@@ -20,3 +22,35 @@ mrn <- select(pts, Medical.Record.Number) %>%
 # print MRN list for EDW
 edw.mrn <- str_c(mrn$mrn, collapse = ";")
 print(edw.mrn)
+
+# person identifiers ----
+
+# read in list of encounters
+encounters <- read_data(pt.dir, "identifiers")
+
+# get person id's and re-run EDW encounters
+persons <- select(encounters, Person.ID) %>%
+    distinct
+
+# print person list for EDW
+edw.person <- str_c(persons$Person.ID, collapse = ";")
+print(edw.person)
+
+# encounter list ----
+
+# get list of all encounters
+encounters <- read_edw_data(pt.dir, "encounters")
+
+# get list of TMC inpatient encounters
+encounters.tmc <- filter(encounters, location == "Memorial Hermann Hospital")
+
+inpt.types <- c("Bedded Outpatient", "EC Emergency Center", "Inpatient", 
+                "Inpatient Rehab", "OBS Day Surgery", "OBS Observation Patient")
+encounters.inpt <- filter(encounters, encounter.type %in% inpt.types)
+
+# check number of patients
+tmp <- encounters.inpt %>%
+    distinct(person.id)
+
+no.outpt <- anti_join(encounters, tmp, by = "person.id") %>%
+    distinct(person.id)
